cbd4919e6aef651364adf5b3939e195209ce500b
Venkataraman <92672198+VR-Blackbird@users.noreply.github.com>
2022-10-26 10:51:41 +0530

    name encapsulation

---
+++ name_encapsulation.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self) -> None:
+        self.foo = 10
+        self._bar = 19
+        self.__baz = 20
+
+    def public_method(self):
+        self._private_method()
+
+    def _private_method(self):
+        pass
