4e6a79c66d29e6e732ca16f0cf28887a4629d899
Venkataraman <92672198+VR-Blackbird@users.noreply.github.com>
2022-11-11 21:08:03 +0530

    IStream base

--- abstract_base.py
+++ abstract_base.py
@@ -1,10 +1,29 @@
 from abc import ABCMeta, ABC, abstractmethod
+import io
 
 
 class SomeAbstractClass(ABC):
     @abstractmethod
     def sides(self):
         pass
+
+
+class IStream(metaclass=ABCMeta):
+    @abstractmethod
+    def read(self):
+        pass
+
+    @abstractmethod
+    def write(self, data):
+        pass
+
+
+class SocketStream(IStream):
+    def read(self):
+        print("Read method of abstract base class")
+
+    def write(self, data):
+        print(data)
 
 
 class Triangle(SomeAbstractClass):
@@ -24,3 +43,9 @@ class Pentagon(SomeAbstractClass):
 
 t = Triangle()
 t.sides()
+
+
+IStream.register(io.IOBase)
+
+f = open("helper_files/abstract.txt")
+print(isinstance(f, IStream))
