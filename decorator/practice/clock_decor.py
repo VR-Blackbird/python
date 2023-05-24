0be249fcd1ef15932f92c2173f7f61153873f93c
Venkataraman <92672198+VR-Blackbird@users.noreply.github.com>
2023-05-24 14:13:25 +0530

    clock decor as parameterized

--- decorator/practice/clock_decor.py
+++ decorator/practice/clock_decor.py
@@ -1,18 +1,23 @@
 import time
 
-
-def clock(func):
-    def clocked(*args):
-        t0 = time.perf_counter()
-        result = func(*args)
-        elapsed_time = time.perf_counter() - t0
-        arg_str = ", ".join(repr(arg) for arg in args)
-        print(f"[{elapsed_time:0.8f}s] ({arg_str})")
-        return result
-
-    return clocked
+DEFAULT_FORMAT = "[{elapsed_time:0.8f}s] ({arg_str})"
 
 
-@clock
+def clock(fmt=DEFAULT_FORMAT):
+    def decorate(func):
+        def clocked(*args):
+            t0 = time.perf_counter()
+            result = func(*args)
+            elapsed_time = time.perf_counter() - t0
+            arg_str = ", ".join(repr(arg) for arg in args)
+            print(fmt.format(**locals()))
+            return result
+
+        return clocked
+
+    return decorate
+
+
+@clock(fmt="[{result}] -> {arg_str} : Time taken = {elapsed_time:0.8f}s")
 def factorial(n):
     return 1 if n < 2 else n * factorial(n - 1)
