f2000e6c6c56e250c1db6658e137a33853be1051
Venkataraman <92672198+VR-Blackbird@users.noreply.github.com>
2023-05-28 15:23:50 +0530

    classic example of a strategy design pattern

---
+++ decorator/design_patterns/classic_strategy.py
@@ -0,0 +1,55 @@
+from random import shuffle
+from abc import ABC, abstractmethod
+
+
+class SupportTicket:
+    def __init__(self, customer, description) -> None:
+        self.customer_name = customer
+        self.issue = description
+
+
+class Ordering(ABC):
+    @abstractmethod
+    def create_order(self, list):
+        pass
+
+
+class Fifo(Ordering):
+    def create_order(self, list):
+        return list
+
+
+class Lifo(Ordering):
+    def create_order(self, list):
+        new_list = list.copy()
+        return reversed(new_list)
+
+
+class Random(Ordering):
+    def create_order(self, list):
+        new_list = list.copy()
+        return shuffle(new_list)
+
+
+class CustomerSupport:
+    def __init__(self, processing_strategy) -> None:
+        self.tickets = []
+        self.strategy = processing_strategy
+
+    def create_ticket(self, customer, description):
+        self.tickets.append(SupportTicket(customer, description))
+
+    def process_tickets(self):
+        ticket_list = self.strategy.create_order(self.tickets)
+        for ticket in ticket_list:
+            print(ticket.customer_name)
+
+
+app = CustomerSupport(Lifo())
+
+app.create_ticket("James", "Unable to discover FREs")
+app.create_ticket("Ken", "Attribute error in TPE json")
+app.create_ticket("lohan", "random iusse")
+
+
+app.process_tickets()
