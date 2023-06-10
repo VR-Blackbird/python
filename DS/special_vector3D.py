afe001c200c6914b960fb656a76e123d047cc0b8
Venkataraman <92672198+VR-Blackbird@users.noreply.github.com>
2023-06-10 23:03:16 +0530

    Vector3d class to showcase spl methods

---
+++ DS/special_vector3D.py
@@ -0,0 +1,21 @@
+import math
+
+
+class Vector:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+    def __repr__(self) -> str:
+        return f"Vector({self.x}, {self.y})"
+
+    def __abs__(self):
+        return math.hypot(self.y, self.x)
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
