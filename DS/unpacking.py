e67a9aa02a4953e481df36e724fca9270ad89150
Venkataraman <92672198+VR-Blackbird@users.noreply.github.com>
2023-06-15 11:35:54 +0530

    star unpacking

---
+++ DS/unpacking.py
@@ -0,0 +1,13 @@
+import os
+
+latitude, longitude = (33.9425, -118.408056)
+print(divmod(20, 8))
+t = (12, 18)
+quotient, remainder = divmod(*t)
+_, filename = os.path.split("/home/vagrant/nknsp/new.sh")
+
+
+# Excess Items
+
+a, b, *rest = range(5)
+a, *body, c, d = range(13)
