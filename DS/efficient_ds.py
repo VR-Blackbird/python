bd7b79eff1cc24d2c76c7da6b04bc1fd7390a4b2
Venkataraman <92672198+VR-Blackbird@users.noreply.github.com>
2023-06-18 16:12:09 +0530

    Increased to 1 million values per sequence

--- DS/efficient_ds.py
+++ DS/efficient_ds.py
@@ -2,14 +2,14 @@ from decorator.simple_decor import clock
 import array
 
 
-lf = list(range(100))
+lf = [float(x) for x in range(10**7)]
 
-af = array.array("f", list(range(100)))
+af = array.array("f", list(range(10**7)))
 
 
 @clock.clock
 def fetch_values(sequence):
-    return sequence[90]
+    return sequence[90000]
 
 
 # print(af)
