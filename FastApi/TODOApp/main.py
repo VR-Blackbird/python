ddc46dceaf3b5535959d146d308d4d9ae424be23
Venkataraman <venkataraman1503@gmail.com>
2023-08-27 15:03:27 +0530

    dependency injection into Fast API

--- FastApi/TODOApp/main.py
+++ FastApi/TODOApp/main.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI
-import models
-from database import engine
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
+from models import Base, Todos
+from database import engine, session_local
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/VR/books")
+def read_books(db: db_dependency):
+    return db.query(Todos).all()
