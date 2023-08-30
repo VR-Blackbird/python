f9f99441a1396f1c400aee332f80ba31be51c6e2
Venkataraman <venkataraman1503@gmail.com>
2023-08-30 10:38:43 +0530

    decode credentials with JWT

--- FastApi/TODOApp/routers/auth.py
+++ FastApi/TODOApp/routers/auth.py
@@ -1,19 +1,21 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel, Field
 from models import User
 from datetime import timedelta, datetime
 from starlette import status
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
 from routers import todos
 
 
-router = APIRouter()
+router = APIRouter(prefix="/auth", tags=["auth"])
 SECRET_KEY = "8f17dcb1ab1ef7dc13194f4b75f34182a2712130ab68e762597477587fcccb0c"
 ALGORITHM = "HS256"
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 
 class CreateUserRequest(BaseModel):
@@ -44,7 +46,24 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+def verify_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate user",
+            )
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user"
+        )
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def create_user(db: todos.db_dependency, create_user_request: CreateUserRequest):
     create_user_model = User(
         email=create_user_request.email,
@@ -67,6 +86,8 @@ def get_access_token(
 ):
     user = authenticate_user(form.username, form.password, db)
     if not user:
-        return "Authentication Failed"
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user"
+        )
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
     return {"access_token": token, "token_type": "bearer"}
