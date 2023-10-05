d62646a28812676cb56787901e10aac445a090e2
Venkataraman <venkataraman1503@gmail.com>
2023-10-05 22:11:37 +0530

    Candy distribution

---
+++ practice/practice.py
@@ -0,0 +1,27 @@
+# Candy distrubution
+
+total = 17  # Input
+num_children = 3  # Input
+
+# Initialize a list of zeros for each child
+children = [0] * num_children
+
+# Distribute candies to children
+current_child = 0
+candies = 1
+
+while total > 0:
+    # Distribute candies to the current child
+    if candies <= total:
+        children[current_child] += candies
+        total -= candies
+        candies += 1
+    else:
+        children[current_child] += total
+        total = 0
+
+    # Move to the next child in a circular manner
+    current_child = (current_child + 1) % num_children
+
+# Print the distribution of candies to children
+print(children)
