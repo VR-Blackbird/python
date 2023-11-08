6d5671197355feda731f5f24eac4779e27797097
Venkataraman <venkataraman1503@gmail.com>
2023-11-08 20:51:15 +0530

    port mappings

--- extras/port_mappings.py
+++ extras/port_mappings.py
@@ -1,4 +1,4 @@
-a = [1, 3, 4, 5, 6, 7,9, 11, 17]
+a = [1, 2, 3, 4, 6, 7, 9, 10, 11, 17]
 
 
 new_str = ""
@@ -10,13 +10,14 @@ for i in range(0, len(a)):
             ranges.append(a[i])
             new_str += f"{ranges[0]}-{ranges[-1]}"
         else:
-            new_str += str(a[i])
+            new_str += f"{a[i]}"
         ranges = []
     elif a[i + 1] == a[i] + 1:
         ranges.append(a[i])
     else:
         ranges.append(a[i])
-        new_str += f"{ranges[0]}-{ranges[-1]}," if len(ranges) > 1 else f'{ranges[0]},'
+        new_str += f"{ranges[0]}-{ranges[-1]}," if len(ranges) > 1 else f"{ranges[0]},"
         ranges = []
+
 
 print(new_str)
